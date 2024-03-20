e49a116d2c22712b6ef89e4e16d4ebd8afa258f7
imina-malak <91422019+imina-malak@users.noreply.github.com>
2024-03-20 23:23:11 +0200

    Update ecu_led.c

--- ECU_Layer/LED/ecu_led.c
+++ ECU_Layer/LED/ecu_led.c
@@ -9,48 +9,36 @@
 
 /**
  * @brief: function to initialize led
- * @param: led: pointer to led
+ * @param: led: the struct which has all info about the pin
  * @return the status of the function 
  *            (E_OK):     the function done successfully 
  *            (E_NOT_OK): the function has issue to perform 
  */
 Std_ReturnType led_initialize(const led_t *led){
     Std_ReturnType ret = E_OK;
-    Pin_config_t pin_config= 
-    {
-        .port= led->port_name,
-        .Pin = led->pin,
-        .logic = led->led_status,
-        .direction = OUTPUT };
     if (NULL==led){
         ret = E_NOT_OK;
     }
     else{
-        gpio_pin_initialize(&pin_config);
+        gpio_pin_initialize(&(led->led_config));
     }
     return ret;
 }
 
 /**
  * @brief: function to turn on led
- * @param: led: pointer to led
+ * @param: led: the struct which has all info about the pin
  * @return the status of the function 
  *            (E_OK):     the function done successfully 
  *            (E_NOT_OK): the function has issue to perform 
  */
 Std_ReturnType led_turn_on(const led_t *led){
     Std_ReturnType ret = E_OK;
-    Pin_config_t pin_config= {
-        .port= led->port_name,
-        .Pin = led->pin,
-        .logic = led->led_status,
-        .direction = OUTPUT };
-
     if (NULL==led){
         ret = E_NOT_OK;
     }
     else{
-        gpio_pin_write_logic(&pin_config,HIGH);
+        gpio_pin_write_logic(&(led->led_config),HIGH);
     }
     
     
@@ -59,7 +47,7 @@ Std_ReturnType led_turn_on(const led_t *led){
 
 /**
  * @brief: function to turn off led
- * @param: led: pointer to led
+ * @param: led: the struct which has all info about the pin
  * @return the status of the function 
  *            (E_OK):     the function done successfully 
  *            (E_NOT_OK): the function has issue to perform 
@@ -67,17 +55,11 @@ Std_ReturnType led_turn_on(const led_t *led){
 Std_ReturnType led_turn_off(const led_t *led){
 
     Std_ReturnType ret = E_OK;
-    Pin_config_t pin_config= {
-        .port= led->port_name,
-        .Pin = led->pin,
-        .logic = led->led_status,
-        .direction = OUTPUT };
-
     if (NULL==led){
         ret = E_NOT_OK;
     }
     else{
-        gpio_pin_write_logic(&pin_config,LOW);
+        gpio_pin_write_logic(&(led->led_config),LOW);
 
     }
 
@@ -87,24 +69,18 @@ Std_ReturnType led_turn_off(const led_t *led){
 
 /**
  * @brief: function to toggle led
- * @param: led: pointer to led
+ * @param: led: the struct which has all info about the pin
  * @return the status of the function 
  *            (E_OK):     the function done successfully 
  *            (E_NOT_OK): the function has issue to perform 
  */
 Std_ReturnType led_turn_toggle(const led_t *led){
     Std_ReturnType ret = E_OK;
-    Pin_config_t pin_config= {
-        .port= led->port_name,
-        .Pin = led->pin,
-        .logic = led->led_status,
-        .direction = OUTPUT };
-
      if (NULL==led){
         ret = E_NOT_OK;
     }
     else{
-         gpio_pin_toggle_logic(&pin_config);
+         gpio_pin_toggle_logic(&(led->led_config));
     }   
     
     return ret;
